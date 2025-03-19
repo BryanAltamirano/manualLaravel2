97938415f37f520cffd559517c195fc996148854
BryanAltamirano <bryanaltamiranocuellar@gmail.com>
2025-03-18 23:27:57 -0600

    se agrega proyexto

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,1 @@
+<h1>inicio.blade</h1>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+use App\Http\Controllers\InicioController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/inicio', function () {
+    return view('inicio');
+});
+Route::get('/vista1', function () {
+    return view('vista1');
+});
+Route::get('/vista2', function () {
+    return view('vista2');
+});
+Route::get('/vista3', function () {
+    return view('vista3');
+});
+
+Route::get('/home', [InicioController::class,'index']);
+
+
+Route::get('/vista1', [InicioController::class,'index']);
+Route::get('/vista2', [InicioController::class,'index']);
+Route::get('/vista3', [InicioController::class,'index']);
+
